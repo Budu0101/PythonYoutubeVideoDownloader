6b562c136b0bae92247fdc3d7562040891dc32f8
Budu0101 <andrei.emirates@gmail.com>
2020-10-01 10:27:35 +0300

    Added_GUI

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-import pytube
+from pytube import *
 
 # Importing tkinter module
 from tkinter import *
 
+<<<<<<< HEAD
 # creating root window  for Tkinter
 root = Tk() 
   
 # adding root window title and dimension for Tkinter
 root.title("Python Youtube Video Downloader") 
 root.geometry('350x200')
+=======
+# Passing the url in the pytube function, that is YouTube
+yt = YouTube(video_url)
+>>>>>>> fd68bc7c7fbf9bb18bf7d27e0955f7784b03dec6
 
 # adding a label to the root window 
 lbl = Label(root, text = "Please enter a valid Youtube URL: ") 
 lbl.grid() 
 
+<<<<<<< HEAD
 # adding Entry Field 
 video_url = Entry(root, width=10) 
 video_url.grid(column =1, row =0) 
@@ -39,3 +45,7 @@ btn.grid(column=2, row=0)
 
 
 root.mainloop()
+=======
+# Downloads the .mp4 file into the current directory. You can change it if you want.
+video.download()
+>>>>>>> fd68bc7c7fbf9bb18bf7d27e0955f7784b03dec6
